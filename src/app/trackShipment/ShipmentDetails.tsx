6a6e12eed0080cf881bff6e337639baaced2dc59
suraj235 <suraj@krenovate.com>
2024-11-29 01:30:06 +0530

    ShipmentDetails UI Responsiveness fix

--- src/app/trackShipment/ShipmentDetails.tsx
+++ src/app/trackShipment/ShipmentDetails.tsx
@@ -156,170 +156,147 @@ import React from 'react';
 
 
   return (
-    <div className='p-4 sm:p-20 sm:pt-4 font-manrope max-w-full sm:max-w-[836px] '>
-      <p className='font-manrope font-medium text-lg sm:text-[20px]'>Tracking ID: {trackingData.trackingId}</p>
-      <div className="mt-6 border border-none flex flex-col items-center justify-center w-full sm:w-[836px]  ">
-        {/* header row */}
-        <div className=" grid grid-cols-1 sm:grid-cols-6 bg-[#F8F9FF] text-center rounded-t-md shadow-md font-semibold">
-          {[
-            "Docket No.",
-            "Reference No.",
-            "Transporter",
-            "Actual Delivery Date",
-            "Estimated Delivery Date",
-            "Current Status",
-          ].map((header, index) => (
-            <div
-              key={index}
-              className={`p-4 flex justify-center items-center font-medium text-[14px] border-b ${index === 5 ? '' : 'border-r'
-                } text-[#272727]`}
-            >
-              {header}
-            </div>
-          ))}
+<div className="py-10 md:pb-16 pb-14 md:py-8 px-4 md:px-20 font-manrope text-primary max-w-[1400px] mx-auto">
+  <p className="font-medium text-lg md:text-xl">Tracking ID: {trackingData.trackingId}</p>
+
+  {/* First Section */}
+  <div className="mt-6 shadow-md rounded-lg flex flex-col items-center justify-center w-full md:w-[836px]">
+    {/* Header Row */}
+    <div className="grid w-full grid-cols-1 md:grid-cols-6 bg-[#F8F9FF] text-center font-semibold">
+      {[
+        "Docket No.",
+        "Reference No.",
+        "Transporter",
+        "Actual Delivery Date",
+        "Estimated Delivery Date",
+        "Current Status",
+      ].map((header, index) => (
+        <div
+          key={index}
+          className={`p-4 text-sm md:text-base border-b border-[#DCDCDC] ${index === 5 ? '' : 'border-r'} text-[#272727]`}
+        >
+          {header}
         </div>
-        {/* data row */}
-        <div className="grid grid-cols-1 sm:grid-cols-6 bg-white text-center shadow-sm rounded-b-lg">
-          {[
-            "docketNo",
-            "referenceNo",
-            "transporter",
-            "actualDeliveryDate",
-            "estimatedDeliveryDate",
-            "currentStatus",
-          ].map((key, index) => (
-            <div
-              key={index}
-              className={`p-2 sm:p-4 font-medium text-[12px] sm:text-[14px] text-[#272727] ${index === 5 ? '' : 'border-r'}`}
-            >
-              {trackingData.shipmentInfo[key as keyof typeof trackingData.shipmentInfo]}
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* second section */}
-
-      <div>
-        <div className="mt-4 p-7 shadow-slate-300  shadow-md rounded-b-lg bg-white rounded-lg w-[836px] h-[482px] ">
-          <div className="grid grid-cols-3  gap-x-28">
-            {/* Row 1 */}
-            <div >
-              <h3 className="text-[#272727] line-height-[24px] font-semibold text-[14px] mb-2">Consignee Name</h3>
-              <p className="text-[#414042] text-[14px] line-height-[20px]">
-                {shipmentData.consigneeName.split("\n").map((line, index) => (
-                  <React.Fragment key={index}>
-                    {line}
-                    <br />
-                  </React.Fragment>
-                ))}
-              </p>
-            </div>
-
-            <div>
-              <h3 className="text-[#272727] line-height-[24px] font-semibold text-[14px] mb-2 ">Consignee Contact No.</h3>
-              <p className="text-[#414042] text-[14px] line-height-[20px]">{shipmentData.consigneeContact}</p>
-            </div>
-
-            <div>
-              <h3 className="text-[#272727] line-height-[24px] font-semibold text-[14px] mb-2">Consignee Address</h3>
-              <p className="text-[#414042] text-[14px] line-height-[20px]">
-                {shipmentData.consigneeAddress}
-                <span className="text-red-500 text-sm ml-1 cursor-pointer hover:underline">
-                  see more
-                </span>
-              </p>
-            </div>
-
-            {/* Row 2 */}
-            <div className='pt-12'>
-              <h3 className="text-[#272727] line-height-[24px] font-semibold text-[14px] mb-2">Booking Id</h3>
-              <p className="text-[#414042] text-[14px] line-height-[20px]">{shipmentData.bookingId}</p>
-            </div>
-
-            <div className='pt-12'>
-              <h3 className="text-[#272727] line-height-[24px] font-semibold text-[14px] mb-2">Order Number</h3>
-              <p className="text-[#414042] text-[14px] line-height-[20px]">{shipmentData.orderNumber}</p>
-            </div>
-
-            <div className='pt-12'>
-              <h3 className="text-[#272727] line-height-[24px] font-semibold text-[14px] mb-2">Packet Count</h3>
-              <p className="text-[#414042] text-[14px] line-height-[20px]">{shipmentData.packetCount}</p>
-            </div>
-
-            {/* Row 3 */}
-            <div className='pt-6'>
-              <h3 className="text-[#272727] line-height-[24px] font-semibold text-[14px] mb-2">Pickup Address</h3>
-              <p className="text-[#414042] text-[14px] line-height-[20px]">
-                {shipmentData.pickupAddress}
-                <span className="text-red-500 text-sm ml-1 cursor-pointer hover:underline">
-                  see more
-                </span>
-              </p>
-            </div>
-
-            <div className='pt-6'>
-              <h3 className="text-[#272727] line-height-[24px] font-semibold text-[14px] mb-2">Docket Date</h3>
-              <p className="text-[#414042] text-[14px] line-height-[20px]">{shipmentData.docketDate}</p>
-            </div>
-
-            <div className='pt-6'>
-              <h3 className="text-[#272727] line-height-[24px] font-semibold text-[14px] mb-2">Mode</h3>
-              <p className="text-[#414042] text-[14px] line-height-[20px]">{shipmentData.mode}</p>
-            </div>
-
-            {/* Row 4 */}
-            <div className='pt-6'>
-              <h3 className="text-[#272727] line-height-[24px] font-semibold text-[14px] mb-2">Shipment Weight</h3>
-              <p className="text-[#414042] text-[14px] line-height-[20px]">{shipmentData.shipmentWeight}</p>
-            </div>
-
-            <div className='pt-6'>
-              <h3 className="text-[#272727] line-height-[24px] font-semibold text-[14px] mb-2">POD</h3>
-              <p className="text-gray-900">{shipmentData.pod}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* third section */}
-
-      <div className=" mt-4     ">
-      <div className=" w-[836px] h-[380px] shadow-slate-300  shadow-md rounded-lg">
-        <table className="w-full  ">
-          <thead className="bg-[#F8F9FF] h-[55px]">
-            <tr>
-              <th className="border-none  text-[14px] font-semibold text-[#272727] px-4 py-2 pr-24">Activity Status</th>
-              <th className="border-none text-[14px] font-semibold text-[#272727] px-4 py-2 text-left ">Date & Time</th>
-              <th className="border-none text-[14px] font-semibold text-[#272727] px-4 py-2  text-left">Detailed Update</th>
-            </tr>  
-          </thead>
-          <tbody>
-            {trackingData.activityLog.map((log, index) => (
-              <tr
-                key={index}
-                className="border-b border-gray-200  px-8 py-2"
-              >
-                <td className=" text-gray-700 px-4 py-2">
-                  {log.status}
-                </td>
-                <td className=" text-gray-700 px-4 py-2">
-                  {log.dateTime}
-                </td>
-                <td className=" text-gray-700 px-4 py-2">
-                  {log.update}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      ))}
     </div>
 
-    {/* /timeline */}
-
-    
+    {/* Data Row */}
+    <div className="grid grid-cols-1 md:grid-cols-6 bg-white text-center shadow-sm rounded-b-lg w-full">
+      {[
+        "docketNo",
+        "referenceNo",
+        "transporter",
+        "actualDeliveryDate",
+        "estimatedDeliveryDate",
+        "currentStatus",
+      ].map((key, index) => (
+        <div
+          key={index}
+          className={`p-4 text-[13px] md:text-sm text-[#272727] ${index === 5 ? '' : 'border-r border-[#DCDCDC]'}`}
+        >
+          {trackingData.shipmentInfo[key as keyof typeof trackingData.shipmentInfo]}
+        </div>
+      ))}
     </div>
+  </div>
+
+  {/* Second Section */}
+  <div className="mt-4 p-7 shadow-md rounded-lg bg-white w-full md:w-[836px]">
+  <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-x-28">
+    {/* Row 1 */}
+    <div>
+      <h3 className="text-[#272727] font-semibold text-[14px] mb-2">Consignee Name</h3>
+      <p className="text-[#414042] text-[14px]">
+        {shipmentData.consigneeName.split("\n").map((line, index) => (
+          <React.Fragment key={index}>
+            {line}
+            <br />
+          </React.Fragment>
+        ))}
+      </p>
+    </div>
+    <div>
+      <h3 className="text-[#272727] font-semibold text-[14px] mb-2">Consignee Contact No.</h3>
+      <p className="text-[#414042] text-[14px]">{shipmentData.consigneeContact}</p>
+    </div>
+    <div>
+      <h3 className="text-[#272727] font-semibold text-[14px] mb-2">Consignee Address</h3>
+      <p className="text-[#414042] text-[14px] flex flex-col">
+        {shipmentData.consigneeAddress}
+        <span className="text-red-500 text-sm cursor-pointer hover:underline">
+          see more
+        </span>
+      </p>
+    </div>
+
+    {/* Row 2 */}
+    <div className="md:pt-6 pt-2">
+      <h3 className="text-[#272727] font-semibold text-[14px] mb-2">Booking Id</h3>
+      <p className="text-[#414042] text-[14px]">{shipmentData.bookingId}</p>
+    </div>
+    <div className="md:pt-6 pt-2">
+      <h3 className="text-[#272727] font-semibold text-[14px] mb-2">Order Number</h3>
+      <p className="text-[#414042] text-[14px]">{shipmentData.orderNumber}</p>
+    </div>
+    <div className="md:pt-6 pt-2">
+      <h3 className="text-[#272727] font-semibold text-[14px] mb-2">Packet Count</h3>
+      <p className="text-[#414042] text-[14px]">{shipmentData.packetCount}</p>
+    </div>
+
+    {/* Row 3 */}
+    <div className="md:pt-6 pt-2">
+      <h3 className="text-[#272727] font-semibold text-[14px] mb-2">Pickup Address</h3>
+      <p className="text-[#414042] text-[14px] flex flex-col">
+        {shipmentData.pickupAddress}
+        <span className="text-red-500 text-sm cursor-pointer hover:underline">
+          see more
+        </span>
+      </p>
+    </div>
+    <div className="md:pt-6 pt-2">
+      <h3 className="text-[#272727] font-semibold text-[14px] mb-2">Docket Date</h3>
+      <p className="text-[#414042] text-[14px]">{shipmentData.docketDate}</p>
+    </div>
+    <div className="md:pt-6 pt-2">
+      <h3 className="text-[#272727] font-semibold text-[14px] mb-2">Mode</h3>
+      <p className="text-[#414042] text-[14px]">{shipmentData.mode}</p>
+    </div>
+
+    {/* Row 4 */}
+    <div className="md:pt-6 pt-2">
+      <h3 className="text-[#272727] font-semibold text-[14px] mb-2">Shipment Weight</h3>
+      <p className="text-[#414042] text-[14px]">{shipmentData.shipmentWeight}</p>
+    </div>
+    <div className="md:pt-6 pt-2">
+      <h3 className="text-[#272727] font-semibold text-[14px] mb-2">POD</h3>
+      <p className="text-[#414042] text-[14px]">{shipmentData.pod}</p>
+    </div>
+  </div>
+  </div>
+
+  {/* Third Section */}
+  <div className="mt-4 shadow-md rounded-lg bg-white w-full overflow-x-auto md:w-[836px]">
+    <table className="w-full text-sm md:text-base">
+      <thead className="bg-[#F8F9FF]">
+        <tr>
+          <th className="px-4 py-2 md:w-[25%] text-left">Activity Status</th>
+          <th className="px-4 py-2 md:w-[30%] text-left">Date & Time</th>
+          <th className="px-4 py-2 md:w-[55%] text-left">Detailed Update</th>
+        </tr>
+      </thead>
+      <tbody>
+        {trackingData.activityLog.map((log, index) => (
+          <tr key={index} className="border-b">
+            <td className="px-4 py-2">{log.status}</td>
+            <td className="px-4 py-2">{log.dateTime}</td>
+            <td className="px-4 py-2">{log.update}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+</div>
+
   );
 };
 
